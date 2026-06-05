8b1a92b90bf0aeca007576a4d51e416fef3ac0fd
juliabannick91-lang <juliabannick91@gmail.com>
2026-06-05 12:12:40 +0200

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,1 +1,34 @@
+variable "subscription_id" {
+  description = "Azure Subscription ID"
+  type        = string
+}
 
+variable "resource_group_name" {
+  description = "Name der Azure Resource Group"
+  type        = string
+  default     = "rg-hallo-welt-portfolio"
+}
+
+variable "location" {
+  description = "Azure Region"
+  type        = string
+  default     = "westeurope"
+}
+
+variable "enable_frontdoor" {
+  description = "Optionale Aktivierung von Azure Front Door"
+  type        = bool
+  default     = false
+}
+
+variable "enable_database" {
+  description = "Optionale Aktivierung der Datenbank"
+  type        = bool
+  default     = false
+}
+
+variable "enable_monitoring" {
+  description = "Optionale Aktivierung von erweitertem Monitoring"
+  type        = bool
+  default     = false
+}
