19d44168554d38e809cbb2a5d8337253541cfc24
juliabannick91-lang <juliabannick91@gmail.com>
2026-06-05 12:13:07 +0200

    Add outputs for resource group name and location

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,1 +1,9 @@
+output "resource_group_name" {
+  description = "Name der Resource Group"
+  value       = azurerm_resource_group.main.name
+}
 
+output "location" {
+  description = "Azure Region"
+  value       = azurerm_resource_group.main.location
+}
