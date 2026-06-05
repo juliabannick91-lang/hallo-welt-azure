979ad93e72668c3e0dbede13e383b38d98219c00
juliabannick91-lang <juliabannick91@gmail.com>
2026-06-05 12:10:58 +0200

    Add Terraform configuration for Azure resources

--- infra/main.tf
+++ infra/main.tf
@@ -1,1 +1,27 @@
+terraform {
+  required_version = ">= 1.6.0"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = var.subscription_id
+}
+
+resource "azurerm_resource_group" "main" {
+  name     = var.resource_group_name
+  location = var.location
+
+  tags = {
+    project     = "hallo-welt-azure"
+    environment = "portfolio"
+    managed_by  = "terraform"
+  }
+}
 
